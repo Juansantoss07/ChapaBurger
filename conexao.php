375cce30411536e10ef3ae5f4acc43eeb18096ad
Juan <72050241+Juansantoss07@users.noreply.github.com>
2021-11-18 14:02:41 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,12 +1,12 @@
 <?php
 
-$usuario = 'root';
-$senha = '';
-$database = "";
-$host = '';
-$port = '';
+$usuario = '3827388_projetouni9';
+$senha = 'root_uni9';
+$database = "3827388_projetouni9";
+$host = 'fdb20.awardspace.net';
 
-$conexao = new mysqli($host, $usuario, $senha, $database, $port);
+
+$conexao = new mysqli($host, $usuario, $senha, $database);
 
 if($conexao->connect_errno){
     echo("Falha ao conectar ao banco de dados: " . $conexao->connect_error);
